3d4c473c63ed9192f88e9c9595fe0bd453cd48c7
gensan-64 <genyuu6240@gmail.com>
2021-08-26 09:39:05 +0000

    finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+before_action :ensure_correct_book, only:[:edit]
 
   def show
     @book = Book.find(params[:id])
@@ -49,4 +50,11 @@ class BooksController < ApplicationController
   def book_params
    params.require(:book).permit(:title, :body)
   end
+
+   def ensure_correct_book
+    @book = Book.find(params[:id])
+     unless @book.user == current_user
+     redirect_to books_path
+     end
+   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,16 +14,21 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if@user != current_user
+     redirect_to user_path(current_user.id)
+    end
   end
 
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to user_path(@user.id), notice: "You have updated user successfully."
+      flash[:notice] = 'You have updated user successfully.'
+      redirect_to user_path(@user.id)
     else
-      render "show"
+      render :edit
     end
   end
+
 
   private
   def user_params
