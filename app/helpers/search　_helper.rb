ac994e63bb7a4b206ba3cd4ddfbaaeba269dcfd6
gensan-64 <genyuu6240@gmail.com>
2021-09-02 07:30:27 +0000

    finish 4章

---
+++ app/helpers/search　_helper.rb
@@ -0,0 +1,2 @@
+module Search　Helper
+end
---
+++ test/controllers/search　_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Search　ControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
