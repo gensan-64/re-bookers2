d3f4f35d5f4b13e3b76ff6fda630c9a032da6ce7
gensan-64 <genyuu6240@gmail.com>
2021-09-04 05:47:20 +0000

    finish 5章

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about', as: 'about'
-  get '/search' => 'search#search'
+  get '/search' => 'searches#search'
 
   resources :books do
    resources :book_comments, only: [:create, :destroy]
